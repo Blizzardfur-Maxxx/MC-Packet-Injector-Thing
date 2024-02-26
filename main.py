08d8371540cad05f6b7718081a5c3b41038eaa52
Blizzardfur <111100393+Blizzardfur-Maxxx@users.noreply.github.com>
2024-02-25 20:51:11 -0600

    fix bad comments

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import socket
 import threading
 
-# List to store connected remote sockets
 connected_remote_sockets = []
 
 def send_packets(client_socket, remote_socket):
@@ -10,29 +9,22 @@ def send_packets(client_socket, remote_socket):
             client_data = client_socket.recv(4096)
             if not client_data:
                 break
-
-            # Send the received data from the client to the remote server
+                
             remote_socket.sendall(client_data)
-
-            # Receive the response from the remote server
             remote_data = remote_socket.recv(4096)
             if not remote_data:
                 break
-
-            # Send the response back to the client
+                
             client_socket.sendall(remote_data)
     except Exception as e:
         print("Error in send_packets:", e)
     finally:
-        # Cleanup: Remove the disconnected remote socket from the list
         connected_remote_sockets.remove(remote_socket)
-        # Close the remote socket
         remote_socket.close()
 
 
 def send_message_to_all(message):
     try:
-        # Send the message packet to all connected remote sockets
         for remote_socket in connected_remote_sockets:
             packet = b'\x03' + len(message).to_bytes(2, byteorder='big') + message.encode('utf-16be')
             remote_socket.sendall(packet)
@@ -43,10 +35,7 @@ def handle_commands(remote_socket):
     while True:
         command = input("Enter command: ")
         if command.startswith("message"):
-            # Extract the message from the command
             message = command.split(" ", 1)[1]
-            
-            # Send the message packet to all connected clients
             send_message_to_all(message)
         else:
             print("Invalid command")
@@ -64,14 +53,10 @@ def proxy_server(local_host, local_port, remote_host, remote_port):
         
         remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         remote_socket.connect((remote_host, remote_port))
-
-        # Add the connected remote socket to the list
         connected_remote_sockets.append(remote_socket)
-
         proxy_thread = threading.Thread(target=send_packets, args=(client_socket, remote_socket))
         proxy_thread.start()
 
-        # Start command handling thread for each client
         command_thread = threading.Thread(target=handle_commands, args=(remote_socket,))
         command_thread.start()
 
